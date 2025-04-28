636d2a724061d1a9f57be882be1366ee220af652
akash-borate <27akashborate@gmail.com>
2025-04-28 21:50:00 +0530

    string acc

---
+++ string.js
@@ -0,0 +1,37 @@
+let string1 =
+  "It is a trust that collects money from a number of investors who share a common investment objective and invests the same in equities, bonds, money market instruments and/or other securities. And the income / gains generated from this collective investment is distributed proportionately amongst the investors after deducting applicable expenses and levies, by calculating a scheme’s “Net Asset Value” or NAV. Simply put, the money pooled in by a large number of investors is what makes up a Mutual Fund.";
+
+const spliter = (str) => {
+  let strTemp = str.split("");
+  let count = 0;
+  let temp = "";
+  let ans = [];
+  for (let i = 0; i < strTemp.length; i++) {
+    if (count < 15) {
+      temp += strTemp[i];
+      count++;
+    } else {
+      ans.push(temp);
+      count = 0;
+      temp = "";
+    }
+  }
+  ans.push(temp);
+  return ans;
+};
+
+console.log(spliter(string1));
+
+const spliter2 = (str) => {
+  const ans = [];
+  let slow = 0;
+  let fast = 15;
+  while (slow < str.length) {
+    ans.push(str.substring(slow, fast));
+    fast += 15;
+    slow += 15;
+  }
+  return ans;
+};
+
+console.log(spliter2(string1));
