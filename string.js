708537a484017c71a52ec280772cd2071ce321d9
akash-borate <27akashborate@gmail.com>
2025-05-01 23:00:40 +0530

    longest substring

--- string.js
+++ string.js
@@ -71,3 +71,20 @@ function isPalindrome(str) {
 }
 
 console.log(isPalindrome(str));
+
+
+function longestUniqueSubstring(str) {
+  let set = new Set();
+  let left = 0, maxLen = 0;
+  for (let right = 0; right < str.length; right++) {
+    while (set.has(str[right])) {
+      set.delete(str[left++]);
+    }
+    set.add(str[right]);
+    maxLen = Math.max(maxLen, right - left + 1);
+  }
+  return maxLen;
+}
+
+console.log(longestUniqueSubstring(str));
+
